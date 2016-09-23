3d4e9532e5849596cee5ca5c72e75e5b59e110d1
thesebas <thesebas@thesebas.net>
2016-09-23 11:06:38 +0200

    move logic to class, exec to subfolder, added random delay option

---
+++ src/Lock.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace thesebas\runlock;
+
+
+class Lock {
+    /**
+     * @var \MongoDB\Collection
+     */
+    protected $locks;
+
+    public function __construct($config) {
+        $mongo = new \MongoDB\Client($config['mongo']['host']);
+
+
+        $dbName = $config['mongo']['dbName'];
+        $collectionName = $config['mongo']['collectionName'];
+
+        $this->locks = $mongo->selectCollection($dbName, $collectionName);
+    }
+
+    public function lock($name, $count = 1) {
+        $locks = $this->locks;
+
+        $filter = [
+            '_id' => $name,
+            'count' => ['$lt' => intval($count)]
+        ];
+        $update = [
+            '$inc' => ['count' => 1]
+        ];
+        $opts = ['upsert' => true];
+        try {
+            $updateResult = $locks->updateOne($filter, $update, $opts);
+            return $updateResult->getUpsertedCount() > 0 || $updateResult->getModifiedCount() > 0;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function unlock($name) {
+        $locks = $this->locks;
+
+        $filter = [
+            '_id' => $name,
+            'count' => ['$gt' => 0]
+        ];
+        $update = [
+            '$inc' => ['count' => -1]
+        ];
+
+        $updateResult = $locks->updateOne($filter, $update);
+
+        return $updateResult->getModifiedCount() > 0;
+    }
+}
